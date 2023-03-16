265185472d5abdf34e8e5f3adf8591d74d5324f9
Arsh Arora <arsharora@Arshs-MacBook-Pro.local>
2023-03-15 22:38:33 -0400

    pushe

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,10 +1,16 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
 
 const Navbar = () => {
+  const navigate = useNavigate();
+    function handleClick() {
+        navigate('/');
+      }
+
     return (
         <nav className="bg-teal-600 text-white p-4">
           <div className="container mx-auto flex justify-between items-center">
-            <a href="/Homepage" className="text-2xl font-normal tracking-wider hover:underline flex-1 text-center">Lifting fit</a>
+            <a onClick={handleClick} className="text-2xl font-normal tracking-wider hover:underline flex-1 text-center">Lifting fit</a>
           </div>
         </nav>
       );
